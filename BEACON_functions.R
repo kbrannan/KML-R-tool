8c39617aa539496e41c741ace8f3d1193cc4d2ba
kbrannan <brannan.kevin@deq.state.or.us>
2014-09-30 17:04:29 -0700

    Added functions for getting distances from sample locations to the BEACON beach lines and to snap the locations to the lines

--- BEACON_functions.R
+++ BEACON_functions.R
@@ -1,4 +1,4 @@
-dwnldUnzpBEACONsp <- function(dir.sp = "//deqhq1/tmdl/TMDL_WR/MidCoast/GIS/BacteriaTMDL/Beaches/EPA_BEACON") {
+dwnldUnzpBEACON.kmb.sp <- function(dir.sp = "//deqhq1/tmdl/TMDL_WR/MidCoast/GIS/BacteriaTMDL/Beaches/EPA_BEACON") {
   ## get spatial data from EPS beach monitoring site BEACON
   ## http://watersgeo.epa.gov/beacon2/about.html
   ## downloaded shapefiles USEPA Geospatial downloads at
@@ -20,4 +20,61 @@ dwnldUnzpBEACONsp <- function(dir.sp = "//deqhq1/tmdl/TMDL_WR/MidCoast/GIS/Bacte
   dir.BEACON.shp <- names(sort(sapply(X=list.dirs(path=dir.sp, full.names=TRUE,recursive=TRUE),FUN=nchar),decreasing=TRUE)[1])
   return(dir.BEACON.shp)
 }
+
+subsetBasedOnExtent.kmb.sp <-function(sp.select.ext,sp.select.from) {
+  ##
+  ## put both sp in CRS used by gogle earth of "+init=epsg:4326"
+  sp.select.ext.KML <- spTransform(sp.select.ext,CRS("+init=epsg:4326"))
+  sp.select.from.KML <- spTransform(sp.select.from,CRS("+init=epsg:4326"))
+  ##
+  ## create a polygon from the bounding box of the extent layer to use for selecting
+  select.bbox <- cbind(floor(bbox(sp.select.ext.KML)[,1]),ceiling(bbox(sp.select.ext.KML)[,2]))
+  select.rec <- rbind(c(select.bbox[1,1],select.bbox[2,2]),c(select.bbox[1,2],select.bbox[2,2]),c(select.bbox[1,2],select.bbox[2,1]),c(select.bbox[1,1],select.bbox[2,1]),c(select.bbox[1,1],select.bbox[2,2]))
+  select.sp.rec <- SpatialPolygons(list(Polygons(list(Polygon(select.rec)),1)))
+  proj4string(select.sp.rec) <- CRS("+init=epsg:4326")
+  ##
+  ## select the features that are in the bounding rectangle and return index of features
+  select.vec <- as.vector(gIntersects(select.sp.rec,sp.select.from.KML,byid=TRUE))
+  ## select features using indexes
+  selected.sp <- sp.select.from[select.vec == TRUE,]
+  return(selected.sp)
+}
+
+lineardistance.kmb.sp <- function(sp.points,sp.lines) {
+  
+  ## functions do not work for coords in geographic units 
+  ## transform to Oregon Lambert projection (+init=epsg:2992)
+  sp.points.ft <- spTransform(sp.points,CRS("+init=epsg:2992"))
+  sp.lines.ft  <- spTransform(sp.lines ,CRS("+init=epsg:2992"))
+  
+  ## check distances of stations to beach extent lines
+  shortest.dists <- numeric(nrow(sp.points.ft@data))
+  for (ii in 1:length(shortest.dists)) {
+    shortest.dists[ii] <- gDistance(sp.points.ft[ii,], sp.lines.ft)
+  }
+  df.shortest.dists <- data.frame(site=sp.points.ft@data$site,dist_ft=round(shortest.dists,0))
+  return(df.shortest.dists)
+}
+
+snapPointsToLine.sp.kmb <- function(sp.points,sp.lines,maxDist=NA) {
+
+  ## functions do not work for coords in geographic units 
+  ## transform to Oregon Lambert projection (+init=epsg:2992)
+  sp.points.ft <- spTransform(sp.points,CRS("+init=epsg:2992"))
+  sp.lines.ft  <- spTransform(sp.lines ,CRS("+init=epsg:2992"))
+  ## snap all points no matter how fair away to the lines
+  if(is.na(maxDist)) {
+    sp.points.snap.ft <- snapPointsToLines(points=sp.points.ft,lines=sp.lines.ft)
+  }
+  ## snap points within maxDist from the lines to the lines
+  if(is.numeric(maxDist)) {
+    sp.points.snap.ft <- snapPointsToLines(points=sp.points.ft,lines=sp.lines.ft,maxDist=maxDist)
+  }
+  ## set projection of snapped points
+  proj4string(sp.points.snap.ft) <- proj4string(sp.points.ft)
+  ## transform snapped points to CRS of orginial points
+  sp.points.snap <- spTransform(sp.points.snap.ft,CRS(proj4string(sp.points)))
+  return(sp.points.snap)
+}
+
   
--- stn_location_to_BEACON_beach.R
+++ stn_location_to_BEACON_beach.R
@@ -10,31 +10,29 @@ library(rgeos)
 tmp.stn.dir <- "//deqhq1/tmdl/TMDL_WR/MidCoast/GIS/BacteriaTMDL/Beaches/Layers"
 tmp.BEACON.dir <- "//deqhq1/tmdl/TMDL_WR/MidCoast/GIS/BacteriaTMDL/Beaches/EPA_BEACON"
 
-## shapefiles and dbf files
-tmp.stn.shp <- "stations_in_group_areas"
+## files
+tmp.stn.csv <- "stn_loc.csv"
 tmp.BEACON.beach.ext.shp <- "rad_beach_l"
 tmp.BEACON.dbf <-"beach_attributes.dbf"
 
-
-
 ##
 ## get the sampling location data
-## Get stations shapefile
-tmp.sp.stn <- readShapePoints(paste0(tmp.stn.dir,"/",tmp.stn.shp), proj4string = CRS("+proj=longlat +datum=NAD83"), verbose = FALSE,repair=FALSE)
-## doa a little cleaning and and reformating
-tmp.sp.stn@data <- data.frame(site=as.character(tmp.sp.stn@data[,grep("site",names(tmp.sp.stn@data))]), data_sourc=tmp.sp.stn@data[,grep("data_sourc",names(tmp.sp.stn@data))], lat=tmp.sp.stn@data[,grep("lat",names(tmp.sp.stn@data))],lon=tmp.sp.stn@data[,grep("lon",names(tmp.sp.stn@data))])
+tmp.stn.locs <- read.csv(paste0(tmp.stn.dir,"/",tmp.stn.csv))
+## create and save stations shapefile
+tmp.sp.stn <- SpatialPointsDataFrame(coords=tmp.stn.locs[,c("lon","lat")], data=tmp.stn.locs[,c("site","data_sourc")], proj4string = CRS("+proj=longlat +datum=NAD83"))
+writeOGR(tmp.sp.stn, dsn=".", layer= "beach_stn_location", driver = "ESRI Shapefile")
+## clean up
+rm(tmp.stn.locs)
 
-
+##
 ## get USEPA BEACON spatial data
-tmp.BEACON.shp.dir <- dwnldUnzpBEACONsp(tmp.BEACON.dir)
+tmp.BEACON.shp.dir <- dwnldUnzpBEACON.kmb.sp(tmp.BEACON.dir)
 ## linear extents of all beaches in BEACON data set
 tmp.sp.BEACON.all <- readShapeLines(fn=paste0(tmp.BEACON.shp.dir,"/",tmp.BEACON.beach.ext.shp), proj4string = CRS("+init=epsg:4269"))
 ## get beaches identified as being in Oregon
 tmp.sp.BEACON.OR <- tmp.sp.BEACON.all[grep("^OR",tmp.sp.BEACON.all@data$SRC_FEATID),]
 ## clean-up
 rm(tmp.sp.BEACON.all)
-
-
 ## get BEACONS dbf file for beach attributes
 tmp.BEACON.attr.all <- read.dbf(file=paste0(tmp.BEACON.shp.dir,"/",tmp.BEACON.dbf))
 ## get beaches in OR
@@ -42,48 +40,20 @@ tmp.BEACON.attr.or <- tmp.BEACON.attr.all[tmp.BEACON.attr.all$BEACH_STAT == "OR"
 ## clean up
 rm(tmp.BEACON.attr.all)
 
-## google CRS is "+init=epsg:4326"
-tmp.sp.stn.on.beach.ext.KML <- spTransform(
-  snapPointsToLines(points=tmp.sp.stn.ft,lines=tmp.sp.beach.ext.or.ft,maxDist=300),
-  CRS("+init=epsg:4326"))
-tmp.sp.beach.ext.or.KML <- spTransform(tmp.sp.beach.ext.or,CRS("+init=epsg:4326"))
-
 ##
 ## get the BEACON beach extents that are in the in Mid-Coast
-## out the stations and the Oregon Beacon beach extentsin the same CRS
-tmp.sp.beach.ext.or.KML <- spTransform(tmp.sp.beach.ext.or,CRS("+init=epsg:4326"))
-tmp.sp.stn.KML <- spTransform(tmp.sp.stn,CRS("+init=epsg:4326"))
-## create a polygon from the bounding box of the stations layer to use for clipping
-clip.bbox <- cbind(floor(bbox(tmp.sp.stn.KML)[,1]),ceiling(bbox(tmp.sp.stn.KML)[,2]))
-clip.rec <- rbind(c(clip.bbox[1,1],clip.bbox[2,2]),c(clip.bbox[1,2],clip.bbox[2,2]),c(clip.bbox[1,2],clip.bbox[2,1]),c(clip.bbox[1,1],clip.bbox[2,1]),c(clip.bbox[1,1],clip.bbox[2,2]))
-clip.sp.rec <- SpatialPolygons(list(Polygons(list(Polygon(clip.rec)),1)))
-proj4string(clip.sp.rec) <- CRS("+init=epsg:4326")
-## select the beach extents that are in the stations layer bounding block
-## this is "clipping" in the geometric set sense
-tmp.vec.in <- as.vector(gIntersects(clip.sp.rec,tmp.sp.beach.ext.or.KML,byid=TRUE))
-## beach extents in Mid-Coast by selecting rows of Oregeon beach extents
-tmp.sp.beach.ext.or.mc.KML <- tmp.sp.beach.ext.or.KML[tmp.vec.in == TRUE,]
+tmp.sp.BEACON.OR.mc  <- subsetBasedOnExtent.kmb.sp(tmp.sp.stn,tmp.sp.BEACON.OR)
+## clean up
+rm(tmp.sp.BEACON.OR)
+
+
+##
+## get distances of sampling locations to the beach lines
+tmp.df.dist.stn.beach.line <- lineardistance.kmb.sp(tmp.sp.stn,tmp.sp.BEACON.OR.mc)
 ##
 ## snap sample location to beach extents
-## transform to Oregon Lambert projection
-tmp.sp.beach.ext.or.mc.ft <- spTransform(tmp.sp.beach.ext.or.mc.KML,CRS("+init=epsg:2992"))
-tmp.sp.stn.ft <- spTransform(tmp.sp.stn,CRS("+init=epsg:2992"))
-## check distances of stations to beach extent lines
-shortest.dists <- numeric(nrow(tmp.sp.stn.ft))
-for (i in seq_len(nrow(tmp.sp.stn.ft))) {
-  shortest.dists[i] <- gDistance(tmp.sp.stn.ft[i,], tmp.sp.beach.ext.or.mc.ft)
-}
-df.shortest.dists <- data.frame(site=tmp.sp.stn.ft$site,dist_ft=round(shortest.dists,0))
-summary(df.shortest.dists)
+tmp.sp.stn.snap <- snapPointsToLine.sp.kmb(tmp.sp.stn,tmp.sp.BEACON.OR.mc,maxDist=NA)
 
-# there are 18 sites that are >= 100 ft from a beach extent
-length(df.shortest.dists$dist_ft[df.shortest.dists$dist_ft > 1000])
-df.shortest.dists[df.shortest.dists$dist_ft > 1000,]
-
-
-
-tmp.sp.stn.on.ext.ft <- snapPointsToLines(points=tmp.sp.stn.ft,lines=tmp.sp.beach.ext.or.mc.ft,maxDist=5280)
-tmp.sp.stn.on.ext.KML <- spTransform(tmp.sp.stn.on.ext.ft,CRS("+init=epsg:4326"))
 
 
 kml(tmp.sp.stn.KML,labels=site, size=10, scale=1)
